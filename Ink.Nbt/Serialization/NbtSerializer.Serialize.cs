146ee180b32500379fdce65429388a6dbcb78ac5
GasInfinity <me@gasinfinity.dev>
2025-01-12 01:38:21 +0100

    feat!: add NbtSerializer SerializeObject

--- Ink.Nbt/Serialization/NbtSerializer.Serialize.cs
+++ Ink.Nbt/Serialization/NbtSerializer.Serialize.cs
@@ -30,7 +30,7 @@ public static partial class NbtSerializer
         typeInfo.Converter.Write(writer, value);
     }
 
-    public static void Serialize<D, T>(Stream stream, string? name, T value, NbtTypeInfo<T> typeInfo, NbtSerializerOptions options)
+    public static void Serialize<D, T>(Stream stream, string? name, T value, NbtSerializerOptions options)
         where D : struct, INbtDatatypeWriter<D>
     {
         using NbtWriter<D> nbtWriter = new(stream, new(options.MaxDepth, options.ShouldValidate));
@@ -62,5 +62,40 @@ public static partial class NbtSerializer
         }
 
         throw new NotImplementedException($"Missing nbt converter for type '{typeof(T).FullName}'");
+    }
+
+    public static void SerializeObject<D>(Stream stream, string? name, object value, NbtSerializerOptions options)
+        where D : struct, INbtDatatypeWriter<D>
+    {
+        using NbtWriter<D> nbtWriter = new(stream, new(options.MaxDepth, options.ShouldValidate));
+        Serialize(nbtWriter, name, value, options);
+    }
+
+    public static void SerializeObject<D>(IBufferWriter<byte> writer, string? name, object value, NbtSerializerOptions options)
+        where D : struct, INbtDatatypeWriter<D>
+    {
+        using NbtWriter<D> nbtWriter = new(writer, new(options.MaxDepth, options.ShouldValidate));
+        Serialize(nbtWriter, name, value, options);
+    }
+
+    public static void SerializeObject<D>(NbtWriter<D> writer, string? name, object value, NbtSerializerOptions options)
+        where D : struct, INbtDatatypeWriter<D>
+    {
+        if(name == null)
+            writer.WriteUnnamedProperty();
+        else
+            writer.WriteProperty(name);
+
+        Type type = value.GetType();
+        foreach(var converter in options.Converters)
+        {
+            if(converter.IsConverterFor(type))
+            {
+                converter.WriteObject(writer, value!);
+                return;
+            }
+        }
+
+        throw new NotImplementedException($"Missing nbt converter for type '{type.FullName}'");
     }
 }
